b240c19c1cd1904a4b2165903db8f474b5dadac7
Marta Jimenez <martajm18@gmail.com>
2022-11-10 21:59:57 +0100

    Revert "esto ya"
    This reverts commit acdfa0c0f427f6e8dacbce7421dca5ab5f0844ac.

--- forg_party/Assets/Script/GameManager.cs
+++ forg_party/Assets/Script/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -18,7 +19,7 @@ public class GameManager : MonoBehaviour
 
     public void Fin()
     {
-
+        SceneManager.LoadScene(0);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
